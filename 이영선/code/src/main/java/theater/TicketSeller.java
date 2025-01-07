03fc25683897db805fc22f1da83c0ff4f5e94ab7
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-01-07 21:25:56 +0900

    refactor: TicketSeller의 sellTo() 캡슐화

--- 이영선/code/src/main/java/theater/TicketSeller.java
+++ 이영선/code/src/main/java/theater/TicketSeller.java
@@ -10,4 +10,17 @@ public class TicketSeller {
     public TicketOffice getTicketOffice() {
         return ticketOffice;
     }
+
+    public void sellTo(Audience audience) {
+        Ticket ticket = ticketOffice.getTicket();
+        if (audience.getBag().hasInvitation()){
+            audience.getBag().setTicket(ticket);
+            return;
+        }
+        if (audience.getBag().hasAmount(ticket.getFee())){
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
 }
